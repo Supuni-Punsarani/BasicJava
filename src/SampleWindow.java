bbc4e37f1948868840ed4a20d5a4fdc876c38aff
Supuni-Punsarani <114279717+Supuni-Punsarani@users.noreply.github.com>
2023-10-10 13:37:41 +0530

    My window

--- src/SampleWindow.java
+++ src/SampleWindow.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class SampleWindow {
+/* Sample Program: Displaying a Window
+File: SampleWindow.java */
+import javax.swing.*;
+class SampleWindow {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(500, 200);
+        myWindow.setTitle("Supuni");
+        myWindow.setVisible(true);
+    }
 }
+
