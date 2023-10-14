df2bba8f8355a95e332fbc9f527f2d2c13fdfde0
Supuni-Punsarani <114279717+Supuni-Punsarani@users.noreply.github.com>
2023-10-14 23:16:43 +0530

    string operations

--- src/SampleWindow.java
+++ src/SampleWindow.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class SampleWindow{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
         String firstName;
         System.out.print("Enter your first name: ");
         firstName = scanner.next( );
@@ -11,21 +11,17 @@ class SampleWindow{
         String lastName;
         System.out.println("enter your last name:");
         lastName=scanner.next();
-
-        //---------
-        System.out.println("Nice to meet you, "+lastName.substring(0,1)+"." + middleName.substring(0,1)+"."+firstName);*/
+        System.out.println("Nice to meet you, "+lastName.substring(0,1)+"." + middleName.substring(0,1)+"."+firstName);
         System.out.println("shopping list:");
         System.out.println("   Apple");
         System.out.println("   Apple");
         System.out.println("   Apple");
 
-         //--------
         String country;
         country = "Sri Lanka";
         System.out.print(country.indexOf("Lanka"));
         System.out.println(country.length());
 
-         //----------
         String java;
         java = "I Love Java and Java loves me.";
         System.out.println(java.indexOf("love"));
