346f60b6aad27ffe4dd080c52a6c5097e6f16799
Supuni-Punsarani <114279717+Supuni-Punsarani@users.noreply.github.com>
2023-10-14 23:12:35 +0530

    string operations

--- src/SampleWindow.java
+++ src/SampleWindow.java
@@ -12,7 +12,24 @@ class SampleWindow{
         System.out.println("enter your last name:");
         lastName=scanner.next();
 
-        System.out.println("Nice to meet you, "+lastName.substring(0,1)+"." + middleName.substring(0,1)+"."+firstName);
+        //---------
+        System.out.println("Nice to meet you, "+lastName.substring(0,1)+"." + middleName.substring(0,1)+"."+firstName);*/
+        System.out.println("shopping list:");
+        System.out.println("   Apple");
+        System.out.println("   Apple");
+        System.out.println("   Apple");
+
+         //--------
+        String country;
+        country = "Sri Lanka";
+        System.out.print(country.indexOf("Lanka"));
+        System.out.println(country.length());
+
+         //----------
+        String java;
+        java = "I Love Java and Java loves me.";
+        System.out.println(java.indexOf("love"));
+
     }
 }
 
