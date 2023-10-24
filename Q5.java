8b07e58ed8d9b06588a92c4f8d2d37c6d5252d3e
Supuni Wannigma <114279717+Supuni-Punsarani@users.noreply.github.com>
2023-10-24 14:25:12 +0530

    Lab 2

--- Q5.java
+++ Q5.java
@@ -1,11 +1,42 @@
-import javax.swing.*;
+import java.util.*;
 
-class Q5 {
-    public static void main(String[]args){
-        JFrame myWindow = new JFrame();
-        myWindow.setSize(300,200);
-        myWindow.setTitle("My First Frame");
-        myWindow.setLocation(50,50);
-        myWindow.setVisible(true);
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your sentence:");
+        String sentence = scanner.nextLine();
+        String sanitizedSentence = sanitizeString(sentence);
+        boolean Palindrome = Palindrome(sanitizedSentence);
+        System.out.println(sanitizedSentence);
+        System.out.println("Is this sentence a palindrome? " + Palindrome);
+    }
+
+    private static String sanitizeString(String s) {
+        s = s.toLowerCase();
+
+        StringBuilder result = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c)) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    private static boolean Palindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
